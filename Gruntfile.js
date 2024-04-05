e40bf8dcb32984e20f8980a21c66933df154657e
Steliann <steliann@tete-chercheuse.fr>
2024-04-05 18:55:23 +0200

    Fix grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,22 +10,13 @@ module.exports = function(grunt) {
       },
       build: {
         src: "src/L.Control.Locate.js",
-        dest: "dist/L.Control.Locate.min.js"
+        dest: "dist/L.Control.Locate.js"
       }
     },
     sass: {
       dist: {
         options: {
           style: "compressed"
-        },
-        files: {
-          "dist/L.Control.Locate.min.css": "src/L.Control.Locate.scss",
-          "dist/L.Control.Locate.mapbox.min.css": "src/L.Control.Locate.mapbox.scss"
-        }
-      },
-      uncompressed: {
-        options: {
-          style: "expanded"
         },
         files: {
           "dist/L.Control.Locate.css": "src/L.Control.Locate.scss",
@@ -40,13 +31,11 @@ module.exports = function(grunt) {
           "package.json",
           "bower.json",
           "dist/L.Control.Locate.css",
-          "dist/L.Control.Locate.min.css",
-          "dist/L.Control.Locate.min.css.map",
+          "dist/L.Control.Locate.css.map",
           "dist/L.Control.Locate.mapbox.css",
-          "dist/L.Control.Locate.mapbox.min.css",
-          "dist/L.Control.Locate.mapbox.min.css.map",
-          "dist/L.Control.Locate.min.js",
-          "dist/L.Control.Locate.min.js.map"
+          "dist/L.Control.Locate.mapbox.css.map",
+          "dist/L.Control.Locate.js",
+          "dist/L.Control.Locate.js.map"
         ],
         push: false
       }
